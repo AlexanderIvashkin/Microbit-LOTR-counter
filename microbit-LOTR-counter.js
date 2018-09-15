9b6abecbd70a8f235703f7f2e5470ef1a742b3fe
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-09-15 23:37:17 +0200

    [bugfix] Blinking formula and other minor things. Working version.

--- microbit-LOTR-counter.js
+++ microbit-LOTR-counter.js
@@ -1,14 +1,15 @@
 let x = 0
-let isInProgress = false
-let isDisplay = true
+let isInProgress = 0
+let isDisplay = false
+let InProgress = false
 let brt = 0
 input.onPinPressed(TouchPin.P1, () => {
     brt = (brt + 32) % 256
     led.setBrightness(brt)
 })
 input.onButtonPressed(Button.A, () => {
-    isInProgress = !(isInProgress)
-    if (!(isInProgress)) {
+    InProgress = !(InProgress)
+    if (InProgress) {
         x += 1
     }
     DrawRounds()
@@ -20,22 +21,23 @@ input.onButtonPressed(Button.B, () => {
     DrawRounds()
     isDisplay = true
 })
-function DrawRounds() {
+function DrawRounds()  {
     basic.clearScreen()
     for (let j = 0; j <= x; j++) {
         led.plot((j - 1) % 5, (j - 1) / 5)
     }
 }
+isDisplay = true
 brt = 255
 basic.forever(() => {
     if (input.pinIsPressed(TouchPin.P2)) {
-        isDisplay=false
+        isDisplay = false
         for (let i = 0; i < 25; i++) {
             led.plotBrightness(Math.random(5), Math.random(5), Math.random(2) * Math.random(256))
         }
     } else {
-        if (isInProgress && isDisplay) {
-            led.toggle(x % 5, x / 5)
+        if (InProgress && isDisplay) {
+            led.toggle((x-1) % 5, (x-1) / 5)
             basic.pause(500)
         }
     }
