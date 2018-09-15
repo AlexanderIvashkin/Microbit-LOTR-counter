6175bfedf88a12f94aa0d77340474e4ca7aef07d
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-09-15 21:04:52 +0200

    No pause in the light show

--- microbit-LOTR-counter.js
+++ microbit-LOTR-counter.js
@@ -17,7 +17,7 @@ input.onButtonPressed(Button.B, () => {
     basic.showNumber(x)
     DrawRounds()
 })
-function DrawRounds() {
+function DrawRounds()  {
     basic.clearScreen()
     for (let j = 0; j <= x; j++) {
         led.plot((j - 1) % 5, (j - 1) / 5)
@@ -27,8 +27,7 @@ brt = 255
 basic.forever(() => {
     if (input.pinIsPressed(TouchPin.P2)) {
         for (let i = 0; i < 25; i++) {
-            led.plotBrightness(Math.random(5), Math.random(5), Math.random(2)*Math.random(256))
+            led.plotBrightness(Math.random(5), Math.random(5), Math.random(2) * Math.random(256))
         }
-        basic.pause(100)
     }
 })
