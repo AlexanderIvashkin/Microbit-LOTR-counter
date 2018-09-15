c663f5dac66f2f18c0d92dddaf55a5b74cd14d64
AlexanderIvashkin <alexander.ivashkin@icloud.com>
2018-09-15 21:56:01 +0200

    No light show

--- microbit-LOTR-counter.js
+++ microbit-LOTR-counter.js
@@ -4,11 +4,6 @@ input.onPinPressed(TouchPin.P0, () => {
     brt = (brt + 32) % 256
     led.setBrightness(brt)
 })
-input.onPinPressed(TouchPin.P2, () => {
-    for (let i = 0; i < 25; i++) {
-        led.plotBrightness(Math.random(4), Math.random(4), Math.random(255))
-    }
-})
 input.onButtonPressed(Button.A, () => {
     x += 1
     DrawRounds()
@@ -25,9 +20,5 @@ function DrawRounds()  {
 }
 brt = 255
 basic.forever(() => {
-    if (input.pinIsPressed(TouchPin.P2)) {
-        for (let i = 0; i < 25; i++) {
-            led.plotBrightness(Math.random(5), Math.random(5), Math.random(2) * Math.random(256))
-        }
-    }
+    led.toggle((x - 1) % 5, (x - 1) / 5)
 })
